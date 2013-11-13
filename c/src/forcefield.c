2aa713a417ce9595dd5578482961fd405306f310
jotinha <jotinha@github>
2013-11-13 13:41:20 +0000

    merged lennardjones into forcefield

--- c/src/forcefield.c
+++ c/src/forcefield.c
@@ -1,8 +1,23 @@
+#include <math.h>
 #include "core.h"
 #include "config.h"
-#include "lennardjones.h"
+double calcEner_r2(double r2, double eps, double sigma, int doVirial, double * virial) {
 
-#define calcEner_r2 ljener_r2
+
+double sr2,sr4,sr6,sr12;
+
+  sr2 = sigma*sigma/r2;
+  sr4 = sr2*sr2;
+  sr6 = sr4*sr2;
+  sr12 = sr6*sr6;
+
+  if (doVirial) {
+    *virial = 4.0*eps*(12.0*sr12 - 6.0*sr6);
+  }
+
+  return 4.0*eps*(sr12-sr6);
+
+}
 
 double calcEner(Config * conf) {
 
--- c/src/lennardjones.h
+++
@@ -1,1 +0,0 @@
-double ljener_r2(double r2, double eps, double sigma, int doVirial, double * virial);
