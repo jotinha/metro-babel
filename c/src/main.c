551a45f4e7fbf611601ef656d193b436645cc736
jotinha <jotinha@github>
2013-11-20 15:39:36 +0000

    c > progress in main

--- c/src/main.c
+++ c/src/main.c
@@ -2,14 +2,44 @@
 #include "random.h"
 #include "montecarlo.h"
 
+struct Files{
+  FILE * eners;
+} FILES;
+
 void init() {
 
   //initialize pseudo random number generator
   randomInit(123);
 
+  //open files
+  FILES.eners = fopen('eners.dat','wt');
+
 }
 
-void load(Config * conf, Simulation * sim) {
+void end() {
+
+  fclose(FILES.eners);
+
+}
+
+void readInput(Config * conf, Simulation * sim,
+           ulong * niters_eq,ulong * step_eq,
+           ulong * niters, ulong * step) {
+
+  fprintf('Temperature: ');
+  fscanf('%f',&sim->temp);
+
+  fprintf('Number of iterations for equilibration: ');
+  fscanf('%i',niters_eq);
+
+  fprintf('Equilibration step: ');
+  fscanf('%i',step_eq);
+
+  fprintf('Number of iterations for run: ');
+  fscanf('%i',niters);
+
+  fprint('Sampling step: ');
+  fscanf('%i',step);
 
 }
 
@@ -22,11 +52,17 @@ void run(int isEquilibration, ulong niters,ulong step, Config * conf, Simulation
 	for (i=0; i < niters; i+=step) {
 		acc = mcstep(conf,sim);
 
-		sample(conf,sim);
-
 		if (isEquilibration) { //adjust parameter
 			sim->maxd *= ( acc >= 0.5 ? 1.05 : 0.95 );
 		}
+
+    if (!isEquilibration) sample(conf,sim);
+
+    if (isEquilibration) {
+      printf('%8i | %14.6f | %14.6f\n',i,sim->maxd,sim->ener);
+    } else {
+      printf('%8i | %14.6f\n',i,sim->ener);
+    }
 
 	}
 
@@ -34,17 +70,27 @@ void run(int isEquilibration, ulong niters,ulong step, Config * conf, Simulation
 
 void sample(Config * conf, Simulation * sim) {
 
+  fprintf(FILES.eners,'%f',sim->ener);
+
 }
 
 void main() {
 
-  Config conf;
-  Simulation sim;
+  Config theconf;
+  Simulation thesim;
+  Config * conf = &(theconf);
+  Simulation * sim = &(thesim);
+  ulong niters_eq, niters, step_eq, step;
 
   init();
 
-  load(conf,sim,);
+  readInput(conf,sim,&niters_eq,&step_eq,&niters,&step);
 
-  run(true,sim->niters_eq, )
+  //equilibration
+  run(true,niters_eq, step_eq,conf,sim);
+  //run
+  run(false,niters, step,conf,sim);
+
+  end();
 
 }
