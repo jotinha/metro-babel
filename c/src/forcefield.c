449155cc3bace108b7ff628d308b5aa04a100feb
jotinha <jotinha@github>
2013-11-21 22:36:30 +0000

    c > removed eps,sigma. use reduced units instead

--- c/src/forcefield.c
+++ c/src/forcefield.c
@@ -2,20 +2,20 @@
 #include "core.h"
 #include "config.h"
 
-double calcEner_r2(double r2, double eps, double sigma, int doVirial, double * virial) {
+double calcEner_r2(double r2, int doVirial, double * virial) {
 
   double sr2,sr4,sr6,sr12;
 
-  sr2 = sigma*sigma/r2;
+  sr2 = 1.0/r2;
   sr4 = sr2*sr2;
   sr6 = sr4*sr2;
   sr12 = sr6*sr6;
 
   if (doVirial) {
-    *virial = 4.0*eps*(12.0*sr12 - 6.0*sr6);
+    *virial = 4.0*(12.0*sr12 - 6.0*sr6);
   }
 
-  return 4.0*eps*(sr12-sr6);
+  return 4.0*(sr12-sr6);
 
 }
 
@@ -23,7 +23,6 @@ double calcEner(Config * conf) {
 
   int i,j;
   double *p1,*p2;
-  double eps = conf->eps, sigma = conf->sigma;
   int N = conf->N;
   double r2;
   double ener = 0, virial = 0, virialij;
@@ -34,7 +33,7 @@ double calcEner(Config * conf) {
       
       r2 = minimg_r2(p1, p2, conf->box);
       
-      ener += calcEner_r2(r2,eps,sigma,1,&virialij);
+      ener += calcEner_r2(r2,1,&virialij);
       virial += virialij;
 
     }
@@ -47,7 +46,6 @@ double calcEner_i(Config * conf, uint i) {
   
   int j;
   double *p1,*p2;
-  double eps = conf->eps, sigma = conf->sigma;
   int N = conf->N;
   double ener = 0, virial = 0, virialij;
   double r2;
@@ -58,7 +56,7 @@ double calcEner_i(Config * conf, uint i) {
     
     r2 = minimg_r2(p1, p2, conf->box);
     
-    ener += calcEner_r2(r2,eps,sigma,1,&virialij);
+    ener += calcEner_r2(r2,1,&virialij);
     virial += virialij;
 
   }
--- c/src/metro.h
+++ c/src/metro.h
@@ -5,7 +5,6 @@ typedef struct Config {
   uint N;
   double * pos;
   double * box;
-  double eps, sigma;
 } Config;
 
 typedef struct Simulation {
