1c6e231774a94895aec564fd20808aa9032f6975
jotinha <jotinha@github>
2013-12-05 18:46:54 +0000

    haskell > having a got at input

--- haskell/src/metro.hs
+++ haskell/src/metro.hs
@@ -4,6 +4,32 @@ import RandomUtils
 import qualified Montecarlo as MC
 import Structures
 
+data Query a = Query {
+	question :: String,
+	answer :: a
+} deriving (Show, Eq)
+
+questions= [Query "Temperature" 1.0,
+			Query "Number of particles" 108,
+			Query "Density" 1.0,
+			Query "Number of interations for equilibration" 100,
+			Query "Equilibration step" 10,
+			Query "Number of iterations for run" 300,
+			Query "Sampling step" 10]
+
+--readAnswer query = do 
+--	qq <- question query
+--	putStr qq ++ ": "
+--	qa <- getLine
+--	q2 <- Query qq (read qa :: (answer query))
+--	return q2
+
+--readInput = mapM (\q -> do 
+--	putStr (question q) ++ ": "
+--	ans <- getLine
+--	return (Query (question q) (read ans :: Int))) questions
+
+
 n = 108 :: Int
 temp = 1.0 :: Double
 rd = 0.1 :: Double
