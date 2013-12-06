90193b135177178fd7fb9f8c9a5cc1dc2cd60f3c
jotinha <jotinha@github>
2013-12-06 00:49:32 +0000

    hs > reads input

--- haskell/src/metro.hs
+++ haskell/src/metro.hs
@@ -3,49 +3,41 @@ import Utils
 import RandomUtils
 import qualified Montecarlo as MC
 import Structures
+import System.IO
 
-data Query a = Query {
-	question :: String,
-	answer :: a
-} deriving (Show, Eq)
+askInput :: (Read b,Show b) => String -> b -> IO b
+askInput question def = do 
+	putStr (question ++ ": ")
+	hFlush stdout		--fix buffer not flushing until end of line
+	ans <- getLine
+	let (err,val) = case reads ans of
+			[(x,"")] 	-> (False,x)	  --if read works, return answer
+			_					-> (True,def)		--if read fails, return default value
+	putStrLn $ (show val) ++ (if err then " *Read Error* Using default" else "")
+	return val
 
-questions= [Query "Temperature" 1.0,
-			Query "Number of particles" 108,
-			Query "Density" 1.0,
-			Query "Number of interations for equilibration" 100,
-			Query "Equilibration step" 10,
-			Query "Number of iterations for run" 300,
-			Query "Sampling step" 10]
-
---readAnswer query = do 
---	qq <- question query
---	putStr qq ++ ": "
---	qa <- getLine
---	q2 <- Query qq (read qa :: (answer query))
---	return q2
-
---readInput = mapM (\q -> do 
---	putStr (question q) ++ ": "
---	ans <- getLine
---	return (Query (question q) (read ans :: Int))) questions
-
-
-n = 108 :: Int
-temp = 1.0 :: Double
-rd = 0.1 :: Double
-dens = 1.0 :: Double
-seed = 12345
-rc = 2.5
-
-rl = (fromIntegral n / dens)**(1.0/3.0)
-
-box = replicate 3 rl
-rndg0 = mkStdGen seed
-
-state0 = let (pos,rndg1) = iniStructureLiq n box rndg0
-         in MC.iniState pos box (rc*rc) rndg1
-
-chain = MC.createChain 10 temp rd (rc*rc) state0 
 
 main = do
-  mapM (\state-> print (MC.energy state)) $ take 5 chain 
+	temp 			<- askInput "Temperature" (1.0 :: Double)
+	n 				<- askInput "Number of particles" (108 :: Int)
+	dens 			<- askInput "Density" (1.0 :: Double)
+	rc 				<- askInput "Potential Cutoff Radius" (2.5 :: Double) 
+	niters_eq <- askInput "Number of interations for equilibration" (100 :: Integer)
+	step_eq		<- askInput	"Equilibration step" (10 :: Int)
+	niters 		<- askInput "Number of iterations for run" (300 :: Integer)
+	step			<- askInput "Sampling step" (10 :: Int)
+	seed			<- askInput "Random Number Generator seed" (1 :: Int)
+
+	let 	
+		rl = (fromIntegral n / dens)**(1.0/3.0)
+		rd = rl*0.01
+		box = replicate 3 rl
+		rndg0 = mkStdGen seed
+
+		state0 = let (pos,rndg1) = iniStructureLiq n box rndg0
+		         in MC.iniState pos box (rc*rc) rndg1
+
+		chain = MC.createChain step temp rd (rc*rc) state0 
+		nobs = (fromIntegral niters) `div` (fromIntegral step)		--integral division
+
+	mapM (\state-> print (MC.energy state)) $ take nobs chain 
