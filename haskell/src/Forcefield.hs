3325f54e74331c86fa7f79fde9252d4e2763390b
jotinha <jotinha@github>
2013-12-09 17:14:10 +0000

    hs > attempt refactor Forcefield loops

--- haskell/src/Forcefield.hs
+++ haskell/src/Forcefield.hs
@@ -21,16 +21,28 @@ lenjones_r2 r2 = 4*(_r2^6 - _r2^3) where _r2 = 1.0/r2
 doubleLoopNoRepeat xs = [(a,b) | (i,a) <- zip [0..] xs, (j,b) <- zip [0..] xs, j > i]
 singleLoopExceptIdx xs i = [x | (j,x) <- zip [0..] xs, j /= i]
 
+doubleLoop funacc funpair x:xs = funacc (singleLoop funacc (funpair x) xs)) (doubleLoop funpair xs)
+doubleLoop funacc funpair [] = 0
+
+singleLoop funacc funone x:xs = funacc x (singleLoop xs)
+singleLoop funacc funone [] = 0
+
 allPairs_r2 pos box = map (\(a,b) -> minimg_r2 a b box) (doubleLoopNoRepeat pos)
 
 enerPair p0 p1 box rcsq  = if (r2 < rcsq) then lenjones_r2 r2 else 0.0
           where r2 = minimg_r2 p0 p1 box
 
-enerTotal pos box rcsq = sum $ map enerPair' (doubleLoopNoRepeat pos)
-          where enerPair' (p0,p1) = enerPair p0 p1 box rcsq
+--enerTotal pos box rcsq = sum $ map enerPair' (doubleLoopNoRepeat pos)
+--          where enerPair' (p0,p1) = enerPair p0 p1 box rcsq
 
-enerOne i pos box rcsq = sum $ map enerPair' (singleLoopExceptIdx pos i)
-          where enerPair' p0 = enerPair p0 (pos !! i ) box rcsq
+--enerOne i pos box rcsq = sum $ map enerPair' (singleLoopExceptIdx pos i)
+          --where enerPair' p0 = enerPair p0 (pos !! i ) box rcsq
 
-enerOnePos pos1 posrest box rcsq = sum $ map enerPair' posrest
-          where enerPair' p0 = enerPair p0 pos1 box rcsq
+--enerOnePos pos1 posrest box rcsq = sum $ map enerPair' posrest
+          --where enerPair' p0 = enerPair p0 pos1 box rcsq
+
+enerTotal pos box rcsq = doubleLoop (+) enerPair' pos
+  where enerPair' p0 p1 = enerPair p0 p1 box rcsq
+
+enerOnePos p1 posrest box rcsq = singleLoop (+) enerPair' posrest
+  where enerPair' p0 = enerPair p0 p1 box rcsq
