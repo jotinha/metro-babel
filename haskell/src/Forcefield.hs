4ddf72d16284db799e7ae1cc4d806c645206354f
jotinha <jotinha@github>
2013-12-09 18:03:48 +0000

    hs > double loops in forcefields now apply function directly

--- haskell/src/Forcefield.hs
+++ haskell/src/Forcefield.hs
@@ -2,7 +2,6 @@ module Forcefield(
   enerTotal,
   enerOnePos,
   enerOne,
-  doubleLoopNoRepeat,
   singleLoopExceptIdx,
   allPairs_r2,
 ) where
@@ -18,19 +17,20 @@ lenjones_dr r = 4*(_r^12 - _r^6) where _r = 1.0/r
 lenjones_r2 0.0 = error "lenjones got zero distance"
 lenjones_r2 r2 = 4*(_r2^6 - _r2^3) where _r2 = 1.0/r2
 
-doubleLoopNoRepeat xs = [(a,b) | (i,a) <- zip [0..] xs, (j,b) <- zip [0..] xs, j > i]
+mapDoubleLoopNoRepeat funpair xs   = [funpair a b | (i,a) <- zip [1..] xs, b <- drop i xs]
+mapSingleLoopExceptIdx funone xs i = [funone x | (j,x) <- zip [0..] xs, j /= i]
+
 singleLoopExceptIdx xs i = [x | (j,x) <- zip [0..] xs, j /= i]
 
-allPairs_r2 pos box = map (\(a,b) -> minimg_r2 a b box) (doubleLoopNoRepeat pos)
+allPairs_r2 pos box = mapDoubleLoopNoRepeat (\a b -> minimg_r2 a b box) pos
 
 enerPair p0 p1 box rcsq  = if (r2 < rcsq) then lenjones_r2 r2 else 0.0
           where r2 = minimg_r2 p0 p1 box
 
-enerTotal pos box rcsq = sum $ map enerPair' (doubleLoopNoRepeat pos)
-          where enerPair' (p0,p1) = enerPair p0 p1 box rcsq
+enerTotal pos box rcsq = sum $ mapDoubleLoopNoRepeat (\a b -> enerPair a b box rcsq) pos
 
-enerOne i pos box rcsq = sum $ map enerPair' (singleLoopExceptIdx pos i)
-          where enerPair' p0 = enerPair p0 (pos !! i ) box rcsq
+enerOne i pos box rcsq = sum $ mapSingleLoopExceptIdx (\a -> enerPair a b box rcsq) pos i
+          where b = pos !! i
 
 enerOnePos pos1 posrest box rcsq = sum $ map enerPair' posrest
           where enerPair' p0 = enerPair p0 pos1 box rcsq
